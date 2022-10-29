91713517593f5207e272fc7a57a377dbeaed4acb
VartoSss <ngaseneger@gmail.com>
2022-10-29 15:58:20 +0500

    client v2.1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -41,47 +41,26 @@ else
         if (round.StartTimestamp < utcNow && utcNow < round.EndTimestamp)
             currentRound = round.Id;
     }
-
-    /*
-    Console.WriteLine($"Нажми ВВОД, чтобы получить первые 50 взятых командой задач типа {taskType} в раунде {currentRound}");
-    Console.ReadLine();
-    Console.WriteLine("Ожидание...");
-    var firstTasks = await challengeClient.GetTasksAsync(currentRound, taskType, TaskStatus.Pending, 0, 50);
-    for(var i = 0; i < firstTasks.Count; i++)
-    {
-        var task = firstTasks[i];
-        Console.WriteLine($"  Задание {i + 1}, статус {task.Status}");
-        Console.WriteLine($"  Формулировка: {task.UserHint}");
-        Console.WriteLine($"                {task.Question}");
-        Console.WriteLine();
-        
-        var answer = Solver.Solve(task);
-        Console.WriteLine($"Нажми ВВОД, чтобы ответить на полученную задачу самым правильным ответом: {answer}");
-        Console.ReadLine();
-        Console.WriteLine("Ожидание...");
-        var updatedTask = await challengeClient.CheckTaskAnswerAsync(task.Id, answer);
-        Console.WriteLine($"  Новое задание, статус {updatedTask.Status}");
-        Console.WriteLine($"  Формулировка:  {updatedTask.UserHint}");
-        Console.WriteLine($"                 {updatedTask.Question}");
-        Console.WriteLine($"  Ответ команды: {updatedTask.TeamAnswer}");
-        Console.WriteLine();
-        if (updatedTask.Status == TaskStatus.Success)
-            Console.WriteLine($"Ура! Ответ угадан!");
-        else if (updatedTask.Status == TaskStatus.Failed)
-            Console.WriteLine($"Похоже ответ не подошел и задачу больше сдать нельзя...");
-        Console.WriteLine();
-        Console.WriteLine("----------------");
-        Console.WriteLine();
-
-        Console.WriteLine($"Нажми ВВОД, чтобы завершить работу программы");
-        Console.ReadLine();
-    }
-    */
     
     Console.WriteLine("----------------");
     Console.WriteLine();
     Console.WriteLine("Введите количество задач");
     var taskAmount = int.Parse(Console.ReadLine());
+    Console.WriteLine("Отправить решение с ручной проверкой адекватности ответов? y\\n");
+    var isHandCheck = Console.ReadLine();
+    if (isHandCheck == "y")
+        await AnswerNTasksWithHandCheck(challengeClient, taskType, currentRound, taskAmount);
+    else if (isHandCheck == "n")
+        await AnswerNTasksWithoutHandCheck(challengeClient, taskType, currentRound, taskAmount);
+    else
+        throw new Exception("Если написано ввести y\\n, то надо б**н написать y или n");
+
+    Console.WriteLine($"Нажми ВВОД, чтобы завершить работу программы");
+    Console.ReadLine();
+}
+
+static async Task AnswerNTasksWithHandCheck(ChallengeClient challengeClient, string taskType, string currentRound, int taskAmount)
+{
     for (var i = 0; i < taskAmount; i++)
     {
 
@@ -115,6 +94,37 @@ else
         Console.WriteLine("----------------");
         Console.WriteLine();
     }
-    Console.WriteLine($"Нажми ВВОД, чтобы завершить работу программы");
-    Console.ReadLine();
+}
+
+static async Task AnswerNTasksWithoutHandCheck(ChallengeClient challengeClient, string taskType, string currentRound, int taskAmount)
+{
+    for (var i = 0; i < taskAmount; i++)
+    {
+        var newTask = await challengeClient.AskNewTaskAsync(currentRound, taskType);
+        Console.WriteLine($"  Новое задание, статус {newTask.Status}");
+        Console.WriteLine($"  Формулировка: {newTask.UserHint}");
+        Console.WriteLine($"                {newTask.Question}");
+        Console.WriteLine();
+        Console.WriteLine("----------------");
+        Console.WriteLine();
+
+        var answer = Solver.Solve(newTask);
+
+        var updatedTask = await challengeClient.CheckTaskAnswerAsync(newTask.Id, answer);
+        Console.WriteLine($"  Новое задание, статус {updatedTask.Status}");
+        Console.WriteLine($"  Формулировка:  {updatedTask.UserHint}");
+        Console.WriteLine($"                 {updatedTask.Question}");
+        Console.WriteLine($"  Ответ команды: {updatedTask.TeamAnswer}");
+        Console.WriteLine();
+        if (updatedTask.Status == TaskStatus.Success)
+            Console.WriteLine($"Ура! Ответ угадан!");
+        else if (updatedTask.Status == TaskStatus.Failed)
+        {
+            Console.WriteLine($"Похоже ответ не подошел и задачу больше сдать нельзя...");
+            throw new Exception("Ты пр****л 200 баллов");
+        }
+        Console.WriteLine();
+        Console.WriteLine("----------------");
+        Console.WriteLine();
+    }
 }
