8f8253a12598942c5207a61fc63967dd4010929a
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-10-30 01:00:10 +0500

    create steganography tests

--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -31,3 +31,16 @@ public class SolverTests
         // Действия, которые будут выполнены после каждого теста
     }
 }
+
+[TestFixture]
+public class SteganographyTests
+{
+    [TestCase("V\r\nhello world\r\nno one's here\r\noh hell", "one")]
+    [TestCase("IX\r\nand one more test for this peace of\r\ni don't even know what to right\r\noh i'll shape one word\r\ni think siiuuuuu won't be said by anyone because\r\nits messiiiuuuuuuu", "messi")]
+    [TestCase("VIII\r\nlol lolly\r\nand again\r\nokay i pull up\r\nuuuuuiis\r\nsiiiiiiiiiuuuuuuuu\r\nwhat is it going to be\r\nahahahahahah\r\nhahahahahaha", "lipsi ha")]
+    [TestCase("XIV\r\nprivet no ti prohodish mimo\r\nya spryatala ulibku\r\nmne vazhno chtobi ti uznal\r\nsecret lovimii kazhdiy ritm\r\nti mne ne beznazlichen", "putin")]
+    public static void SteganographyTest(string task, string expectedResult)
+    {
+        Assert.AreEqual(expectedResult, Solver.SolveSteganography(task));
+    }
+}
