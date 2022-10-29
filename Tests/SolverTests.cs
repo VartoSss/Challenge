3cc60a6887ded1ef8c3ee2917ba1a22237f153d7
VartoSss <ngaseneger@gmail.com>
2022-10-29 23:47:33 +0500

    create moment tests

--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -17,10 +17,8 @@ public class SolverTests
     public void Solve_Returns42_Everytime()
     {
         var expected = "42";
-        TaskResponse task = null;
-
-        var actual = Solver.Solve(task);
-
+        var task = "asdsffdgdfsdsfdfs";
+        var actual = Solver.SolveStarter(task);
         Assert.AreEqual(expected, actual);
     }
 
@@ -30,4 +28,20 @@ public class SolverTests
         // Разборка тестового окружения
         // Действия, которые будут выполнены после каждого теста
     }
+}
+
+[TestFixture]
+public class MomentTests
+{
+    
+    [TestCase("17:08:10 28.10.2022", "28 октября 17:08")]
+    [TestCase("21:00:59 31.12.2022", "31 декабря 21:00")]
+    [TestCase("14:35:25 28.10.2022", "28 октября 14:35")]
+    [TestCase("11:00:03 17.12.1998", "17 декабря 11:00")]
+    [TestCase("15:35:50 28.10.2022", "28 октября 15:35")]
+    public void MomentTest(string task, string expectedResult)
+    {
+        Assert.AreEqual(expectedResult, Solver.SolveMoment(task));
+    }
+
 }
