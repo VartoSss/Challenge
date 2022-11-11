c31ec65ceb4a88ffb4b510a470d59cf89634a161
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-11-12 03:55:56 +0500

    new cypher

--- ConsoleApp/Cypher.cs
+++ ConsoleApp/Cypher.cs
@@ -23,31 +23,76 @@ public class Cypher
         else if (splittedTask.Length == 2 && splittedTask[0].Substring(0, 14) == "Caesar's code=")
         {
             var chars = new char[]
-        {
+            {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
             'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
             '4', '5', '6', '7', '8', '9', '\'', ' '
-        };
+            };
 
-        //поменяй тут пж чтобы принимало с сайта
-        var str = splittedTask[1];
-        var shift = int.Parse(splittedTask[0].Substring(14));
+            //поменяй тут пж чтобы принимало с сайта
+            var str = splittedTask[1];
+            var shift = int.Parse(splittedTask[0].Substring(14));
 
-        var result = new StringBuilder();
+            var result = new StringBuilder();
 
-        for (var i = 0; i < str.Length; i++)
+            for (var i = 0; i < str.Length; i++)
+            {
+                var index = (Array.IndexOf(chars, str[i]) - shift) % chars.Length;
+                if (index < 0)
+                    index = chars.Length + index;
+                var newSymbol = chars[index];
+                result.Append(newSymbol.ToString());
+            }
+            //и тут на return)
+            return result.ToString();
+        }
+
+        else if (splittedTask[0].Substring(0, 20) == "prime multiplicator=")
         {
-            var index = (Array.IndexOf(chars, str[i]) - shift) % chars.Length;
-            if (index < 0)
-                index = chars.Length + index;
-            var newSymbol = chars[index];
-            result.Append(newSymbol.ToString());
-        }
-        //и тут на return)
-        return result.ToString();
-        }
+            var chars = new char[]
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                '4', '5', '6', '7', '8', '9', '\'', ' '
+            };
 
+            var result = new StringBuilder();
+
+            var taskParts = splittedTask[0].Split(": ");
+            var formula = taskParts[1];
+            var multiplicator = new StringBuilder();
+            for (var i = 20; i < taskParts[0].Length; i++)
+            {
+                if (char.IsDigit(taskParts[0][i]))
+                    multiplicator.Append(taskParts[0][i]);
+                else
+                    break;
+            }
+
+            var intMultiplicator = int.Parse(multiplicator.ToString());
+
+            if (formula == "(multiplicator * (charIndex + 1)) % (|ABC| + 1) - 1")
+            {
+                var alphabetPower = chars.Length;
+                for (var i = 0; i < splittedTask[1].Length; i++)
+                {
+                    var currentIndex = Array.IndexOf(chars, splittedTask[1][i]);
+                    for (var j = 0; j < chars.Length; j++)
+                    {
+                        if ((intMultiplicator * (j + 1) % (chars.Length + 1) - 1) == currentIndex)
+                        {
+                            result.Append(chars[j].ToString());
+                            break;
+                        }
+                    }
+                }
+                return result.ToString();
+            }
+            else
+                throw new Exception("new formula in cypher task");
+        }
         else
             throw new Exception("AAAAAA TASK GOT HARDER");
     }
--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -70,7 +70,8 @@ public class CypherTests
     [TestCase("#reversed#uuuis uuuis uuuis  uuuis#", "siuuu  siuuu siuuu siuuu")]
     [TestCase("#Caesar's code=-22#3u u7pwu9p43up9xu8upau7up9xupr4408ps439qy3y3wp54au7v'1#", "never get one these were the books containing powerful")]
     [TestCase("#Caesar's code=-9#j7ej723j28fi2pfl2di2gfkk7i2 2k'fl9'k29ip88 e6fi2d73ek#", "sense as for you mr potter i thought gryffindor meant")]
-    [TestCase("#Caesar's code=1#hpjohaupaljmmazpvaupojhiuarvjssfmmatobqqfeaijtagjohfst#", "")]
+    [TestCase("#Caesar's code=1#hpjohaupaljmmazpvaupojhiuarvjssfmmatobqqfeaijtagjohfst#", "going to kill you tonight quirrell snapped his fingers")]
+    [TestCase("#prime multiplicator=8419 formula: (multiplicator * (charIndex + 1)) % (|ABC| + 1) - 1#dc4rnedc4rnernqeclqe7e c0062rne4nrrevx722n0veq ne07q#", "goyle goyle let out a horrible yell scabbers the rat")]
     public static void CypherTest(string task, string expectedResult)
     {
         Assert.AreEqual(Cypher.SolveCypher(task), expectedResult);
