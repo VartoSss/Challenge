6909e8eab26625c6e3d9453826738f2acfce3160
VartoSss <ngaseneger@gmail.com>
2022-11-11 20:37:17 +0500

    Cypher modified but not tested

--- ConsoleApp/Cypher.cs
+++ ConsoleApp/Cypher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp;
@@ -19,6 +20,32 @@ public class Cypher
                 result.Append(splittedTask[1][i]);
             return result.ToString();
         }
+        else if (splittedTask.Length == 2 && splittedTask[0].Substring(0, 14) == "Caesar's code=")
+        {
+            var chars = new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '\'', ' '
+        };
+
+        //поменяй тут пж чтобы принимало с сайта
+        var str = splittedTask[1];
+        var shift = int.Parse(splittedTask[0].Substring(13));
+
+        var result = new StringBuilder();
+
+        for (var i = 0; i < str.Length; i++)
+        {
+            var index = (Array.IndexOf(chars, str[i]) - shift) % chars.Length;
+            var newSymbol = chars[index];
+            result.Append(newSymbol.ToString());
+        }
+        //и тут на return)
+        return result.ToString();
+        }
+
         else
             throw new Exception("AAAAAA TASK GOT HARDER");
     }
