eaae8bcbb5acb2899959155fcc6aecd66ada7498
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-11-12 14:05:35 +0500

    file romans update

--- ConsoleApp/Romans.cs
+++ ConsoleApp/Romans.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace ConsoleApp;
+
+public class Romans
 {
-    internal class Class1
+    public static short RomanToArab(string roman)
     {
+        short result = 0;
+        var RomToArab = new Dictionary<char, short>
+            {{ 'I', 1 },{ 'V', 5 },{ 'X', 10 },{ 'L', 50 },{ 'C', 100 },{ 'D', 500 },{ 'M', 1000 } };
+        for (short i = 0; i < roman.Length - 1; ++i)
+        {
+            if (RomToArab[roman[i]] < RomToArab[roman[i + 1]])
+                result -= RomToArab[roman[i]];
+            else if (RomToArab[roman[i]] >= RomToArab[roman[i + 1]])
+                result += RomToArab[roman[i]];
+        }
+        return result += RomToArab[roman[^1]];
     }
 }
