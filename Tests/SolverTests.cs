52e5f38153ae72c6800db51829140719c2256c9b
VartoSss <ngaseneger@gmail.com>
2022-11-12 14:21:12 +0500

    Math with romans done and tested

--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -55,7 +55,8 @@ public class MathTests
     [TestCase("((7+5+3*9)-(7%3))/3", "12")]
     [TestCase("((10-1)*(7%13-14))+((0+14/13)-(9+3)+(3/7))*((6+2+8-3)/9+(8+12))%12", "-66")]
     [TestCase("((4*8-2)*(11+11+1))*((7/9+1+6)-(12/11-2%6)-(4/5))/10", "552")]
-    [TestCase("((CXVII*LIV-DCCLXVI-XLV)/DCCCLXXXIV)+((DXXIX+CMXXXIX/CXVIII)-(CCCLI+DCXXXI*CXCI-DCLV))", "55")]
+    [TestCase("((CXVII*LIV-DCCLXVI-XLV)/DCCCLXXXIV)+((DXXIX+CMXXXIX/CXVIII)-(CCCLI+DCXXXI*CXCI-DCLV))", "-119675")]
+    [TestCase("((117*54-776-45)/884)+((529+939/118)-(351+631*191-655))", "-119675")]
     public static void MathTest(string task, string expectedResult)
     {
         Assert.AreEqual(expectedResult, MathSolver.Solve(task));
