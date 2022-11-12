2d1f098715bcb56d4dc7d7d36c718ea90e03afbb
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-11-12 18:02:15 +0500

    shape old version

--- ConsoleApp/Shape.cs
+++ ConsoleApp/Shape.cs
@@ -26,62 +26,31 @@ public class Shape
             cordsList.Add(pairArray);
         }
 
-        var dictionaryY = new Dictionary<int, List<int>>(); // y -> <x1, x2, x3, ..., xn>
-        var dictionaryX = new Dictionary<int, List<int>>(); // x -> <y1, y2, y3, ..., yn>
+        var dictionary = new Dictionary<int, List<int>>(); // y -> <x1, x2, x3, ..., xn>
 
         foreach (var pair in cordsList)
         {
             var x = pair[0];
             var y = pair[1];
-            if (!dictionaryY.ContainsKey(y))
-                dictionaryY.Add(y, new List<int>());
-            dictionaryY[y].Add(x);
-            if (!dictionaryX.ContainsKey(x))
-                dictionaryX.Add(x, new List<int>());
-            dictionaryX[x].Add(y);
+            if (!dictionary.ContainsKey(y))
+                dictionary.Add(y, new List<int>());
+            dictionary[y].Add(x);
         }
 
-        var yValues = dictionaryY.Keys.ToArray();
-        var xValues = dictionaryX.Keys.ToArray();
+        var yValues = dictionary.Keys.ToArray();
         Array.Sort(yValues);
-        Array.Sort(xValues);
 
         var minY = yValues[0];
         var maxY = yValues.Last();
         var medianY = yValues[yValues.Length / 2];
 
-        var minX = xValues[0];
-        var maxX = xValues.Last();
-        var medianX = xValues[xValues.Length / 2];
-
-        var isLikeCircle = dictionaryY[minY].Count == dictionaryY[maxY].Count && dictionaryY[medianY].Count > dictionaryY[maxY].Count;
-
-        var isLikeSquare = dictionaryY[maxY].Count == dictionaryY[minY].Count && dictionaryY[minY].Count == dictionaryY[maxY].Count;
-
-        var isTriangle = (dictionaryY[minY].Count < dictionaryY[medianY].Count && dictionaryY[medianY].Count < dictionaryY[maxY].Count) ||
-            (dictionaryY[minY].Count > dictionaryY[medianY].Count && dictionaryY[medianY].Count > dictionaryY[maxY].Count) ||
-            (dictionaryX[minX].Count < dictionaryX[medianX].Count && dictionaryX[medianX].Count < dictionaryX[maxX].Count) ||
-            (dictionaryX[minX].Count > dictionaryX[medianX].Count && dictionaryX[medianX].Count > dictionaryX[maxX].Count);
-
-        if (isLikeCircle)
+        if (dictionary[minY].Count == dictionary[maxY].Count) // либо круг, либо квадрат
         {
-            var isCircle = isLikeCircle && dictionaryY[medianY].Count == dictionaryX[medianX].Count;
-            if (isCircle)
+            if (dictionary[medianY].Count > dictionary[maxY].Count)
                 return "circle";
-            else
-                throw new Exception("Shape got harder");
+            return "square";
         }
-        else if (isLikeSquare)
-        {
-            var isSquare = isLikeSquare && dictionaryY[minY].Count == dictionaryX[minX].Count;
-            if (isSquare)
-                return "square";
-            else
-                throw new Exception("Shape got harder");
-        }
-        else if (isTriangle)
-            return "equilateraltriangle";
         else
-            throw new Exception("Shape got harder");
+            return "equilateraltriangle";
     }
 }
