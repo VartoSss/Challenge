8694096a7be0f9f2b53e4a8e7e42a7a6ad74734e
VartoSss <ngaseneger@gmail.com>
2022-11-25 21:03:53 +0500

    Math can now parse fields with space or

--- ConsoleApp/MathSolver.cs
+++ ConsoleApp/MathSolver.cs
@@ -4,10 +4,24 @@
     {
         public static string Solve(string question)
         {
-            var withoutFunctions = GetRidOfFunctions.CalculateFunctions(question);
+            var result = "";
+            var possibleSeparators = new[] { " ", "\"" };
+            var splittedQuestion = question.Split(possibleSeparators, System.StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var questionField in splittedQuestion)
+                result += CalculateField(questionField) + " ";
+            var trimmedResult = result.Trim();
+            return trimmedResult;
+        }
+
+        public static string CalculateField(string questionField)
+        {
+            var withoutFunctions = GetRidOfFunctions.CalculateFunctions(questionField);
             var postfixExpression = PostfixBuilder.BuildPostfixExpression(withoutFunctions);
             var answer = PostfixCalculator.Calculate(postfixExpression).ToString();
             return answer;
         }
+
+        
     }
 }
