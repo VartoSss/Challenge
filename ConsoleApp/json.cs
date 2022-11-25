06c5deabc7a1198b960cd4ba3c4deddccebfb342
localhost23 <ssportsprog@gmail.com>
2022-11-26 00:23:39 +0500

    json

---
+++ ConsoleApp/json.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class json
+    {
+        public static string SolveCommonJSON(string question)
+        {
+            var counter = 0;
+            var nums = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            var buildingStr = new StringBuilder();
+            for (var i = 0; i < question.Length; i++)
+            {
+                if (question[i] == '-' || nums.Contains(question[i]))
+                    buildingStr.Append(question[i]);
+                else
+                {
+                    if (!string.IsNullOrEmpty(buildingStr.ToString()))
+                        counter += int.Parse(buildingStr.ToString());
+                    buildingStr = new StringBuilder();
+                }
+            }
+            return counter.ToString();
+        }
+    }
+}
--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -149,3 +149,14 @@ public class ShapeTest
         Assert.AreEqual(expectedResult, NewShape.NewShapeSolver(task));
     }
 }
+
+[TestFixture]
+public class JSONTest
+{
+    [TestCase(@"{""disacknowledgement"":""85"",""allelism"":""-46"",""radiata"":""74"",""commatic"":""-81""}", "32")]
+    [TestCase(@"{""orbicle"":""12"",""exceptio"":""83"",""sufficientness"":""39"",""sizar"":""-61"",""allotypies"":""3"",""echeloned"":{""oropharynxes"":""59"",""pattinsonize"":""-9"",""mormondom"":""-67"",""unsalness"":""-21"",""plethysmographic"":""46"",""unlengthened"":""91""},""gorgeously"":{""unseething"":""-64"",""kohlrabies"":""83"",""inwalling"":""-73"",""aversion"":""87"",""approbativeness"":""2"",""pragmatizer"":""38""},""supracondyloid"":{""scowed"":""-98"",""antichrist"":""62"",""patterer"":""-29"",""tercelet"":""4"",""pellety"":""-57"",""bloodthirstiness"":""44""},""stonewaller"":{""marvin"":""66"",""ovisaclike"":""55"",""fleuretty"":""-98"",""metronomically"":""-13""}}", "184")]
+    public static void JSONTests(string task, string expectedResult)
+    {
+        Assert.AreEqual(expectedResult, json.SolveCommonJSON(task));
+    }
+}
