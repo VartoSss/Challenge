389d69715ddf13f3b7f2e04ee8d534389f386108
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-11-26 00:56:35 +0500

    add str-number tests

---
+++ Tests/StringNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests;
+
+[TestFixture]
+public class StringNumbersTests
+{
+    [TestCase("four hundred ninety-six million five hundred ninety-one thousand six hundred twenty-seven", "496591627")]
+    [TestCase("one", "1")]
+    [TestCase("one million twenty-three", "1000023")]
+    [TestCase("zero", "0")]
+    public void StringNumberTests(string task, string expectedResult)
+    {
+        Assert.AreEqual(expectedResult, StringNumber.StringNumberSolver(task));
+    }
+}
