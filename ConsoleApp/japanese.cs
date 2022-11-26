45e35757ca96fdd6644e307246acf09f411311e7
localhost23 <ssportsprog@gmail.com>
2022-11-26 20:33:47 +0500

    japanese

---
+++ ConsoleApp/japanese.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static IronPython.Modules._ast;
+using System.Diagnostics;
+
+namespace Japan
+{
+    public class japanese
+    {
+        public static string SolveJapanese(string question)
+        {
+            var inputPath = "C:\\Users\\Артем\\H1_1\\ConsoleApp\\Input.txt";
+            var outputPath = "C:\\Users\\Артем\\H1_1\\ConsoleApp\\Output.txt";
+            File.WriteAllText(inputPath, string.Empty);
+            File.WriteAllText(outputPath, string.Empty);
+            var data = question;
+            data = data.Replace("columns:","");
+            data = data.Replace("rows:", " ");
+            var info = data.Split(' ');
+            var str = new StringBuilder();
+            var str2 = new StringBuilder();
+            foreach (var item in info[0])
+            {
+                if (item == ';')
+                    str.Append(',');
+                else if (item == ',')
+                    str.Append(' ');
+                else
+                    str.Append(item);
+            }
+            foreach (var item in info[1])
+            {
+                if (item == ';')
+                    str2.Append(',');
+                else if (item == ',')
+                    str2.Append(' ');
+                else
+                    str2.Append(item);
+            }
+            var str3 = str.ToString().TrimEnd(',');
+            var str4 = str2.ToString().TrimEnd(',');
+            var result = string.Format("[clues]\r\ncolumns={0}\r\nrows={1}", str3, str4);
+            File.WriteAllText(@"C:\Users\Артем\H1_1\ConsoleApp\Input.txt", result);
+            var command = $"pynogram -b {inputPath} >> {outputPath}";
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C "+command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+            var text = new List<string>();
+            using (StreamReader reader = new StreamReader(outputPath,Encoding.Default))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    text.Add(line);
+            }
+            return TextParser(text);
+        }
+    }
+}
---
+++ Tests/japaneseTest.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp;
+using Japan;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class japaneseTest
+    {
+        [TestCase("columns:4;1,1;1;1;1,2;rows:4;1;1,1;1,1;2;", ".####;#....;#...#;#...#;##...")]
+        [TestCase("columns:1;2,1;4;2,1;1;rows:1;3;5;1;1,1;", ".####;#....;#...#;#...#;##...")]
+
+        public static void JapaneseTests(string task, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, japanese.SolveJapanese(task));
+        }
+    }
+}
