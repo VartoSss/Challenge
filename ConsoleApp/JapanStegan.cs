88d8e11da83ffbd5e9df304a632186d255d6bb70
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-12-10 00:40:43 +0500

    not ready yet stegan

---
+++ ConsoleApp/JapanStegan.cs
@@ -0,0 +1,30 @@
+﻿using Japan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp;
+
+public class JapanStegan
+{
+    static Dictionary<char, int> Symbols = new Dictionary<char, int>
+    {
+        { '#', 1 },
+        { '.', 0 }
+    };
+
+    public static string JapanSteganSolver(string task)
+    {
+        var result = Japanese.SolveJapanese(task);
+        var answer = new StringBuilder();
+
+        foreach (var symbol in result)
+        {
+            answer.Append(symbol);
+        }
+
+        throw new Exception("aaaa");
+    }
+}
