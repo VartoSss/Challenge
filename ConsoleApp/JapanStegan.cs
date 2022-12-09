4ba1acdf40c88ebd61224cf2f91022ca0fe46ee7
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2022-12-10 03:26:15 +0500

    japanese-stegan not ready

--- ConsoleApp/JapanStegan.cs
+++ ConsoleApp/JapanStegan.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp;
@@ -17,14 +18,63 @@ public class JapanStegan
 
     public static string JapanSteganSolver(string task)
     {
-        var result = Japanese.SolveJapanese(task);
-        var answer = new StringBuilder();
+        var taskParts = task.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        var japaneseTask = taskParts[0];
+        var taskText = taskParts[1];
 
-        foreach (var symbol in result)
+        var japaneseResult = Japanese.SolveJapanese(japaneseTask);
+        var japaneseRows = japaneseResult.Split(';');
+        var answerInRows = new StringBuilder();
+        var answerInColumns = new StringBuilder();
+
+        for (var i = 0; i < japaneseRows[0].Length; i++)
         {
-            answer.Append(symbol);
+            var currentColumn = new StringBuilder();
+
         }
 
-        throw new Exception("aaaa");
+        foreach (var symbol in japaneseResult)
+            if (Symbols.ContainsKey(symbol))
+                answerInRows.Append(Symbols[symbol]);
+
+        var binaryString = answerInRows.ToString();
+        var reversedBinary = new StringBuilder();
+        for (var i = binaryString.Length - 1; i >= 0; i--)
+            reversedBinary.Append(binaryString[i]);
+
+        var reversedString = reversedBinary.ToString();
+        var binaryList = new List<string>();
+
+        while (!string.IsNullOrEmpty(reversedString))
+        {
+            var currentBinary = reversedString.Substring(0, Math.Min(8, reversedString.Length));
+            binaryList.Add(currentBinary);
+            reversedString = reversedString.Substring(8);
+        }
+
+        var indexes = new List<int>();
+        foreach (var binaryNumber in binaryList)
+            indexes.Add(BinaryToDecimal(binaryNumber));
+
+        var resultString = new StringBuilder();
+        foreach (var index in indexes)
+            resultString.Append(taskText[index % taskText.Length]);
+
+        return resultString.ToString();
+    }
+
+    public static int BinaryToDecimal(string binaryNumber)
+    {
+        var result = 0.0;
+        var power = 0;
+
+        for (var i = binaryNumber.Length - 1; i >= 0; i--)
+        {
+            var digit = binaryNumber[i].ToString();
+            result += int.Parse(digit) * Math.Pow(2, power);
+            power++;
+        }
+
+        return (int)result;
     }
 }
---
+++ Tests/JapanSteganTests.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests;
+
+[TestFixture]
+public class JapanSteganTests
+{
+    [TestCase("columns:1,3;1,3;2,2;1,1,1;3,2;2,2,1;1,1;1,3,1;4,1;rows:1,1,1;5,1;1,1,2;2,6;2,1,2;5,1;1,2,2;;\r\nhard to follow my masters instructions he is a great wizard and i am weak you mean he was there in the classroom", "0")]
+    public static void JapanSteganTest(string result, string expectedValue)
+    {
+        Assert.AreEqual(expectedValue, JapanStegan.JapanSteganSolver(result));
+    }
+}
